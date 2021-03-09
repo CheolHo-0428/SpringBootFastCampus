4b03305e38cccfb7625153d2b477511f72665fb9
CheolHo-0428 <againnr1@gmail.com>
2021-03-09 21:23:57 +0900

    상품 API READ까지 업데이트

---
+++ study/src/main/java/com/example/study/controller/api/UserApiController.java
@@ -0,0 +1,46 @@
+package com.example.study.controller.api;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserApiLogicService userApiLogicService;
+
+
+    @Override
+    @PostMapping("") // /api/user
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> requset) {
+        log.info("{}", requset);
+        return userApiLogicService.create(requset);
+    }
+
+    @Override
+    @GetMapping("{id}") // /api/user/{id}
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
+        log.info("read id : {}", id);
+        return userApiLogicService.read(id);
+    }
+
+    @Override
+    @PutMapping("") // /api/user
+    public Header update(@RequestBody Header<UserApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}") // /api/user/{id}
+    public Header delete(@PathVariable(name = "id") Long id) {
+        return null;
+    }
+}
